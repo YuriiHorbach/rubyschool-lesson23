ddd66d9852543d5edc18219abfb7795c09afdc19
southl <gyrii84@gmail.com>
2019-08-26 12:54:09 +0300

    fixed jquery code loading

--- public/scripts.js
+++ public/scripts.js
@@ -2,4 +2,7 @@ $('#waitYou').css('background-color', 'yellow');
 $('#bbb').css('background-color', 'red');
 $('.ccc').css('background-color', 'blue');
 $('select[name="colorpicker"]').simplecolorpicker();
-$('#color').simplecolorpicker();
+
+$(function(){
+    	$('#color').simplecolorpicker();	
+})
